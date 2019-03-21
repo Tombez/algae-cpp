8ecf2232adc9fa9faa149b307ba4bc9de4c68161
Tom Burris <unsaneity@gmail.com>
2019-03-21 14:54:05 -0700

    Vec2 now more feature-complete

--- src/Vec2.hpp
+++ src/Vec2.hpp
@@ -9,23 +9,57 @@ public:
 	T y;
 	Vec2() {}
 	Vec2(T x, T y) : x(x), y(y) {}
+	Vec2<T>& operator +=(const T b) {
+		x += b;
+		y += b;
+		return *this;
+	}
+	Vec2<T>& operator -=(const T b) {
+		x -= b;
+		y -= b;
+		return *this;
+	}
+	Vec2<T>& operator *=(const T b) {
+		x *= b;
+		y *= b;
+		return *this;
+	}
+	Vec2<T>& operator /=(const T b) {
+		x /= b;
+		y /= b;
+		return *this;
+	}
 	Vec2<T>& operator +=(const Vec2<T>& b) {
 		x += b.x;
 		y += b.y;
 		return *this;
 	}
-	Vec2<T>& operator /=(T b) {
-		x /= b;
-		y /= b;
+	Vec2<T>& operator -=(const Vec2<T>& b) {
+		x -= b.x;
+		y -= b.y;
 		return *this;
 	}
-	Vec2<T>& operator *=(T b) {
-		x *= b;
-		y *= b;
+	Vec2<T>& operator *=(const Vec2<T>& b) {
+		x *= b.x;
+		y *= b.y;
 		return *this;
 	}
-	Vec2<T> operator *(T b) const {
-		return Vec2(x * b, y * b);
+	Vec2<T>& operator /=(const Vec2<T>& b) {
+		x /= b.x;
+		y /= b.y;
+		return *this;
+	}
+	Vec2<T> operator +(const T b) const {
+		return Vec2<T>(x + b, y + b);
+	}
+	Vec2<T> operator -(const T b) const {
+		return Vec2<T>(x - b, y - b);
+	}
+	Vec2<T> operator *(const T b) const {
+		return Vec2<T>(x * b, y * b);
+	}
+	Vec2<T> operator /(const T b) const {
+		return Vec2<T>(x / b, y / b);
 	}
 	Vec2<T> operator +(const Vec2<T>& b) const {
 		return Vec2<T>(x + b.x, y + b.y);
@@ -33,28 +67,43 @@ public:
 	Vec2<T> operator -(const Vec2<T>& b) const {
 		return Vec2<T>(x - b.x, y - b.y);
 	}
-	Vec2<T>& assign(const Vec2<T>& b) {
-		x = b.x;
-		y = b.y;
-		return *this;
+	Vec2<T> operator *(const Vec2<T>& b) const {
+		return Vec2<T>(x * b.x, y * b.y);
+	}
+	Vec2<T> operator /(const Vec2<T>& b) const {
+		return Vec2<T>(x / b.x, y / b.y);
+	}
+	Vec2<T> clone() const {
+		return Vec2<T>(x, y);
 	}
 	Vec2<T>& assign(T nx, T ny) {
 		x = nx;
 		y = ny;
 		return *this;
 	}
-	Vec2<T>& fromAngle(float angle) {
+	Vec2<T>& assign(const Vec2<T>& b) {
+		x = b.x;
+		y = b.y;
+		return *this;
+	}
+	Vec2<T>& fromAngle(const float angle) { // in radians
 		x = std::cos(angle);
 		y = std::sin(angle);
 		return *this;
 	}
-	Vec2<T>& setLength(float len) {
+	Vec2<T>& setLength(const float len) {
 		return *this *= len / this->getLength();
 	}
+	T getLengthSquared() const {
+		return x * x + y * y;
+	}
 	T getLength() const {
-		return std::sqrt(x * x + y * y);
+		return std::sqrt(this->getLengthSquared());
 	}
 	T getDistSquared(const Vec2<T>& b) const {
 		return (x - b.x) * (x - b.x) + (y - b.y) * (y - b.y);
+	}
+	T getDist(const Vec2<T>& b) const {
+		return std::sqrt(this->getDistSquared(b));
 	}
 };
