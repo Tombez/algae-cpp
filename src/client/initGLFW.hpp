a1c60ba2a735c56bfe291cbfed52c344256a95f0
Tom Burris <unsaneity@gmail.com>
2019-09-02 21:01:52 -0700

    enabled alpha

--- src/client/initGLFW.hpp
+++ src/client/initGLFW.hpp
@@ -60,8 +60,8 @@ void init() {
 		exit(glewErr);
 	}
 
-	// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	// glEnable(GL_BLEND);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_MULTISAMPLE);
 
 	if (videoMode != nullptr) {
